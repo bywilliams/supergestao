3a837db23a18e9eee1dabcc3e9a4a08cb406af3a
bywilliams <willian.seu@gmail.com>
2023-08-29 09:38:35 -0300

    1.18 - hotfix: correcao do nome da tabela do model fornecedor

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/* 
+Nome da tabela gerado pelo eloquent sera 'fornecedors'
+porém o nome correto da tabela é 'fornecedores' em PT
+*/
 class Fornecedor extends Model
 {
     use HasFactory;
+
+    protected $table = 'fornecedores'; // declara o nome correto da tabela
+
 }

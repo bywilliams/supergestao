621255c26acf8567c41da2c4c9eea8a55e3b6607
bywilliams <willian.seu@gmail.com>
2023-10-11 15:37:51 -0300

    1.46 - relacionamento 1x1 entre models Produto e ProdutoDetalhe

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -9,4 +9,16 @@ class Produto extends Model
 {
     use HasFactory;
     protected $fillable = ['nome', 'descricao', 'peso', 'unidade_id'];
+
+    public function produtoDetalhe () {
+        return $this->hasOne('App\models\ProdutoDetalhe');
+
+        /**
+         * Verifica se Produto tem um produtoDetalhe
+         * então ele verifica no BD se existe 1 registro relacionado em produto_detalhes (fk) -> produto_id
+         * checando se produtos (pk) -> id existe na tabela produto_detalhes na coluna produto_id
+         */
+
+    }
+
 }
